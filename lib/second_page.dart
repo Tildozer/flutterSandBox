f381de7caf72c2de4d994a0f174dfb5a6a36387f
Tildozer <actninswitch@gmail.com>
2023-04-26 15:43:55 -0600

    got data to pass to components

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,34 +1,32 @@
 import 'package:flutter/material.dart';
+import "components/scrolling_list_view.dart" as horizontal_scroll;
+class Entry{
+  String message; // property
+  String name; // property
+  IconData icon; // prop
+
+  Entry(this.message, this.name, this.icon);
+}
+
 
 class SecondPage extends StatelessWidget {
   const SecondPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final List<Entry> enteries = [
+      Entry("I learned", "How to use", Icons.access_alarm_sharp),
+      Entry("Classes to build", "Objects", Icons.dangerous_sharp)
+    ];
     return Column(
       children: [
         SingleChildScrollView(
-            child: Column(
-          children: [
-            SizedBox(
-              height: 200,
-              child: ListView.builder(
-                itemCount: 10,
-
-                scrollDirection: Axis.horizontal,
-                itemBuilder: ((context, index) => Container(
-                  height: 100,
-                  width: 200,
-                  margin: const EdgeInsets.only(left: 60.0),
-                  decoration:
-                  BoxDecoration(color: Colors.redAccent.shade100),
-                  child: const Text("here"),
-                    )),
-              ),
-            ),
-           
-          ],
-        ))
+          child: Column(
+            children: [
+              horizontal_scroll.Hori(enteries: enteries),
+            ],
+          ),
+        )
       ],
     );
   }
